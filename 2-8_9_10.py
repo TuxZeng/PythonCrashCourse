ce4e54ee0dee104a5a55b4fedfc1be813cbbff58
TuxZeng <TuxZeng@foxmail.com>
2024-01-27 00:08:56 +0800

    P27 28 number and note

---
+++ 2-8_9_10.py
@@ -0,0 +1,13 @@
+#output four fun to show eight.
+print(5+3)
+print(12-4)
+print(2*4)
+print(24/3)
+
+'''
+output my favorite number
+just for fun
+'''
+fnum=66
+msg = f"My favorite number is {fnum}"
+print(msg)
